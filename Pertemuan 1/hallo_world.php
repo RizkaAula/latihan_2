2f3cddeab96a378f342837a6a795280b88731cb4
nuralimah23 <alimahnur2312@gmail.com>
2023-11-16 13:20:21 +0700

    first commit

---
+++ Pertemuan 1/hallo_world.php
@@ -0,0 +1,18 @@
+<?php
+  $nama="Rizka Aula Assaf";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Latihan 1</title>
+  </head>
+  <body>
+    <p><center><h1>!..Halo..!</h1></center></p>
+    <p><center><h2>Saya Sedang belajar HTML</h2></center></p>
+    <?php
+      echo "<h2><center>Saya $nama senang berkenalan dengan anda</h2></center>";
+    ?>
+  </body>
+</html>
